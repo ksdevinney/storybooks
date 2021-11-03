c5d9bbf5f26d3f795b6aa375ffc8619990554975
ksdevinney <katie@devinney.org>
2021-11-02 20:25:30 -0500

    add render to routes

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 // login page
 // GET /
 router.get('/', (req, res) => {
-    res.send('Login')
+    res.render('Login')
 });
 
 // dashboard page
 // GET /dashboard
 router.get('/dashboard', (req, res) => {
-    res.send('Dashboard')
+    res.render('Dashboard')
 });
 
 module.exports = router;
