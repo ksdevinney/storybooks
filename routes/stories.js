a7b7e00305a085b00d4d40d4188c170b2dbf431b
ksdevinney <katie@devinney.org>
2021-11-30 19:55:04 -0600

    display public stories

--- routes/stories.js
+++ routes/stories.js
@@ -23,4 +23,22 @@ router.post('/', ensureAuth, async (req, res) => {
     }
 });
 
+// show all public
+// GET /stories/index
+router.get('/stories', ensureAuth, async (req, res) => {
+    try {
+        const stories = await Story.find({ status: 'public' })
+            .populate('user')
+            .sort({ createdAt: 'desc' })
+            .lean()
+
+        res.render('stories/index', {
+            stories,
+        })
+    } catch (err) {
+        console.error(err);
+        res.render('error/500');
+    }
+});
+
 module.exports = router;
