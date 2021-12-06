04ad79e1d0ab0c6764d378fe29284ad59be0e75b
ksdevinney <katie@devinney.org>
2021-12-05 20:34:45 -0600

    method override middleware

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const connectDB = require('./config/db');
 const passport = require('passport');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
+const methodOverride = require('method-override');
 
 // load config
 dotenv.config({ path: './config.env.test' });
@@ -22,6 +23,15 @@ const app = express();
 //body parser middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//method override
+app.use(methodOverride(function (req, res) {
+    if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+        var method = req.body._method
+        delete req.body._method
+        return method
+    }
+}));
 
 // logging
 if (process.env.NODE_ENV === 'development') {
